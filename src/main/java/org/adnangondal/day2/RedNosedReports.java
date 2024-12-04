ab680a776b632953ffb72a39af35f550fa61a654
AdnanGondal <43793942+AdnanGondal@users.noreply.github.com>
2024-12-04 17:35:09 +0000

    Day 2: Added initial test code and test cases

--- src/main/java/org/adnangondal/day2/RedNosedReports.java
+++ src/main/java/org/adnangondal/day2/RedNosedReports.java
@@ -16,8 +16,38 @@ public class RedNosedReports {
         return safeReports;
     }
 
-    private static boolean isReportSafe(List<Integer> report) {
+    public static boolean isReportSafe(List<Integer> reports) {
+        int n = reports.size();
 
-        return false;
+        if (n == 0){
+            return false;
+        }
+
+        if (n == 1){
+            return true;
+        }
+
+        boolean isIncreasing = reports.get(1)-reports.get(0) > 0 ? true : false;
+
+        for (int i=0;i<n-1;i++){
+            int diff = (reports.get(i+1)-reports.get(i));
+
+            if (isIncreasing){
+                if (diff < 0){
+                    return false;
+                }
+            } else {
+                if (diff > 0){
+                    return false;
+                }
+            }
+
+            if (Math.abs(diff) < 1 || Math.abs(diff) > 3 ){
+                return false;
+            }
+        }
+
+
+        return true;
     }
 }
