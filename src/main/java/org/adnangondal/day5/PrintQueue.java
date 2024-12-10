9d4acb42c3f6ed32e1967f707b29676da9006276
AdnanGondal <43793942+AdnanGondal@users.noreply.github.com>
2024-12-10 18:09:14 +0000

    day5: refactor and use steams

--- src/main/java/org/adnangondal/day5/PrintQueue.java
+++ src/main/java/org/adnangondal/day5/PrintQueue.java
@@ -8,25 +8,18 @@ public class PrintQueue {
 
   public int getSumOfValidMiddlePages(
       List<Pair<String, String>> rules, List<String> pageNumbersList) {
-
-    int sum = 0;
-    for (String pageNumbers : pageNumbersList) {
-      sum += getMidpointIfValid(rules, pageNumbers);
-    }
-    return sum;
+    return pageNumbersList.stream()
+        .mapToInt(pageNumbers -> getMidpointIfValid(rules, pageNumbers))
+        .sum();
   }
 
   protected static int getMidpointIfValid(List<Pair<String, String>> rules, String pageNumbers) {
     if (pageNumbers.isEmpty()) return 0;
     int[] nums = Arrays.stream(pageNumbers.split(",")).mapToInt(Integer::parseInt).toArray();
 
-    for (Pair<String, String> rule : rules){
-      if (!validateRuleAgainstNumbers(rule, nums)){
-        return 0;
-      }
-    }
-
-    return nums[nums.length/2];
+    return rules.stream().allMatch(rule -> validateRuleAgainstNumbers(rule, nums))
+        ? nums[nums.length / 2]
+        : 0;
   }
 
   private static boolean validateRuleAgainstNumbers(Pair<String, String> rule, int[] nums) {
@@ -35,11 +28,11 @@ public class PrintQueue {
     int firstNumIndex = -1;
     int secondNumIndex = -1;
 
-    for (int i=0;i<nums.length ; i++ ){
-      if (nums[i]==firstNum){
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == firstNum) {
         firstNumIndex = i;
       }
-      if (nums[i]==secondNum){
+      if (nums[i] == secondNum) {
         secondNumIndex = i;
       }
     }
